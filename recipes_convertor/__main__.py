ef33dc804ea30c6253ff2a0da5698c64fe99d1ee
Jackson Porter <jackson@jacksonporterjp.com>
2025-06-14 10:54:47 -0600

    init

---
+++ recipes_convertor/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from recipes_convertor.app import main
+
+def init() -> None:
+    if __name__ == "__main__":
+        main()
+
+
+init()
---
+++ recipes_convertor/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def main() -> None:
+    print("Hello, World!")
+
+
+def init() -> None:
+    if __name__ == "__main__":
+        main()
+
+
+init()
